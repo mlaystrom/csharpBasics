abeb048339f359ca65f0e10bfbf7c4e4093fe3ed
Marla Laystrom <marlalaystrom@gmail.com>
2023-05-08 14:26:33 -0400

    created first c# project

---
+++ 00_HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//instead of console.log
+
+// c# is a compiled language
+// javascript is interpreted
+
+// Before running c#, we must first BUILD and COMPILE it (dotnet build in terminal to build our project) then 
+//dotnet run --project 00_HelloWorld (after doing this, will be able to see the "Hello, World!" console statement in terminal)
+
+//dotnet watch run --project 00_HelloWorld
+//dotnet watch provides a hot reload
+//This runs the application after changes are saved (sort of like liveserver, will allow us to run the code without rerunning commands over)
+
+// ctrl + c to stop dotnet watch
+
+//gitignore- specifies intentionally untracked filled to ignore
+
+Console.WriteLine("My name is Marla!");
